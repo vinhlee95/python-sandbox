5333f0b1ff86f4e54f7efb43bf18f11331d374b1
Vinh Le <vinh@vertics.co>
2020-10-12 06:43:23 +0300

    init project

---
+++ project.py
@@ -0,0 +1,15 @@
+name = "Vinh"
+monthly_rent = 1000
+day_rent = monthly_rent / 30
+
+# Printing output
+print("-----PRINT OUPUT-------")
+print(f"My name is {name}")
+print(f"My daily rental is {day_rent}")
+print(type(day_rent))
+
+# Check types
+print("-----CHECKING TYPES-------")
+print(type(name))
+print(type(monthly_rent))
+print(type(day_rent))
