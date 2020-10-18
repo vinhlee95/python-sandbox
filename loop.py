4b0d93c7ff6fe8fd422802ad7a1d4c09ce126f60
Vinh Le <vinh@vertics.co>
2020-10-18 11:07:58 +0300

    feat: add loop

---
+++ loop.py
@@ -0,0 +1,33 @@
+"""
+Loop in Python
+
+📚 Resources:
+https://www.learnpython.dev/02-introduction-to-python/110-control-statements-looping/05-looping-in-python/
+"""
+# 🛠 Loop syntax
+phones = ["iphone", "pixel"]
+accessories = ["charger", "earbuds"]
+
+# for phone in phones:
+# 	print(f"Phone is: {phone}")
+
+# Looping backwards
+# for phone in reversed(phones):
+# 	print(f"Phone is: {phone}")
+
+# 🛠 Loop through a range
+# Range start from "start", but "stop" is not included
+# for number in range(0, 3):
+# 	print(f"Number in range is: {number}")
+
+# 🛠 Loop through a range with "step"
+# for number in range(1, 11, 2):
+# 	print(f"Number in range is: {number}")
+
+# 🛠 Loop over items with the index
+# for index, number in enumerate(range(1, 11, 2)):
+# 	print(f"Number in range is: {number}. Index: {index}")
+
+# 🛠 Looping over 2 collections
+# for phone, accessory in zip(phones, accessories):
+# 	print(phone, accessory)
